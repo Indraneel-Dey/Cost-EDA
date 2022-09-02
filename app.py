55496fe545420999ce7a1be47750eae2f89c8088
Indraneel-Dey <110671577+Indraneel-Dey@users.noreply.github.com>
2022-09-02 13:29:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from streamlit_pandas_profiling import st_profile_report
 from pandas_profiling import ProfileReport
 
 df = pd.read_csv('Cost Pivot.csv')
+df.columns = ['Month', 'Ashirbad Wheat Flour Total Cost', 'Fair and Lovely Face Cream Total Cost', 
+              'Fortune Cooking Oil Total Cost', 'Maggi Sauce Total Cost', 'Potatoes Total Cost', 'Sriram Papad Total Cost', 
+              'Ashirbad Wheat Flour Marginal Cost', 'Fair and Lovely Face Cream Marginal Cost', 'Fortune Cooking Oil Marginal Cost', 
+              'Maggi Sauce Marginal Cost', 'Potatoes Marginal Cost', 'Sriram Papad Marginal Cost']
 profile = ProfileReport(df)
 
 st.write('Indraneel Dey')
